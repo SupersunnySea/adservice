1fdf5e38dd7003764c5e1b469a8a7e1d8d66af46
yang.wang12 <yang.wang12@ucarinc.com>
2019-07-04 11:56:22 +0800

    microservice.py

--- microservices1.py
+++ microservices1.py
@@ -14,53 +14,59 @@ def test_microservices(url):
     option.add_argument('--start-maximized')
     driver = webdriver.Firefox(executable_path='/home/wangyang/Downloads/geckodriver', options=option)
 
-
     driver.get(url)
     wait = WebDriverWait(driver, 50)\
         .until(EC.presence_of_element_located((By.CLASS_NAME, "card-img-top")))
+    print("frontend success")
     driver.find_element_by_class_name('card-img-top').click()
     time.sleep(3)
-    print("1.Check out the details of the goods successfully")
 
-    
-    wait = WebDriverWait(driver, 100)\
-        .until(EC.presence_of_element_located((By.CLASS_NAME, "alert-link")))
-    driver.find_element_by_class_name('alert-link').click()
-    time.sleep(1)
-    handles = driver.window_handles
-
-    driver.switch_to.window(handles[1])
-    wait = WebDriverWait(driver, 100)\
+    #/product/L9ECAV7KIM
+    wait = WebDriverWait(driver, 50)\
         .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-info.btn-lg.ml-3")))
+    print("product success")
     driver.find_element_by_class_name('btn.btn-info.btn-lg.ml-3').click()
     time.sleep(3)
-    print("2.Check out advertisement and add cart successfully")
 
-    driver.switch_to.window(handles[0])
-    wait = WebDriverWait(driver, 100)\
-        .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-info.btn-lg.ml-3")))
-    driver.find_element_by_class_name('btn.btn-info.btn-lg.ml-3').click()
+    #/cart
+    wait = WebDriverWait(driver, 50) \
+         .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-secondary")))
+    print("cart success")
+    wait = WebDriverWait(driver, 50) \
+         .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-primary")))
+    driver.find_element_by_class_name("btn.btn-primary").click()
     time.sleep(3)
-    print("3.Add cart successfully")
-
 
     wait = WebDriverWait(driver, 50) \
          .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-primary")))
-    driver.find_element_by_css_selector("Button.btn.btn-primary").click()
+    print("cart/checkout success")
     time.sleep(3)
-    print("4.Checkout successfully")
-    print("5.Your order is complete! ")
-    print("============================")
+
+    driver.get(url)
+    wait = WebDriverWait(driver, 50)\
+        .until(EC.presence_of_element_located((By.CLASS_NAME, "alert-link")))
+    time.sleep(3)
+    print("frontend success by url")
+    
+    driver.get("http://ad79bcb0f949e11e9a35102cce2aac0b-865086301.ap-southeast-1.elb.amazonaws.com/product/L9ECAV7KIM")
+    wait = WebDriverWait(driver, 50)\
+        .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-info.btn-lg.ml-3")))
+    time.sleep(3)
+    print("product success by url")
+
+    driver.get("http://ad79bcb0f949e11e9a35102cce2aac0b-865086301.ap-southeast-1.elb.amazonaws.com/cart")
+    wait = WebDriverWait(driver, 50) \
+         .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-secondary")))
+    time.sleep(3)
+    print("cart success by url")
+
+    #driver.get("http://ad79bcb0f949e11e9a35102cce2aac0b-865086301.ap-southeast-1.elb.amazonaws.com/cart/checkout")
+    #wait = WebDriverWait(driver, 50) \
+    #     .until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btn-primary")))
+    #time.sleep(3)
 
 if __name__ == '__main__':
 
     url = "http://ad79bcb0f949e11e9a35102cce2aac0b-865086301.ap-southeast-1.elb.amazonaws.com:80/"
     test_microservices(url)
-
-
-
-
-
-
-
 
