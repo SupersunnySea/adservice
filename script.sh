6b6f6d9d347e259e5d5224f7c7685f8890e97885
王昆朋 <kunpeng.wang@luckincoffee.com>
2019-07-04 15:57:17 +0800

    hh

--- script.sh
+++ script.sh
@@ -9,7 +9,10 @@ cat /etc/resolv.conf
 python -V
 
 pwd
+ls /usr/bin/firefox/
 
+ls /usr/local/bin/firefox/
+find  / -name firefox
 pip3 install selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
 
