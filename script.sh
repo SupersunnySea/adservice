197b8a6f14f0bb83c5837b3049b5db3ae12aae26
王昆朋 <kunpeng.wang@luckincoffee.com>
2019-07-04 13:40:55 +0800

    hh

--- script.sh
+++ script.sh
@@ -8,4 +8,6 @@ cat /etc/resolv.conf
 
 python -V
 
- wget ad79bcb0f949e11e9a35102cce2aac0b-865086301.ap-southeast-1.elb.amazonaws.com/product/2ZYFJ3GM2N
+pwd
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
