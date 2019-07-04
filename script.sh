4079c611bf588fe2cefe84c7794fbd6b6bb82289
王昆朋 <kunpeng.wang@luckincoffee.com>
2019-07-04 16:34:48 +0800

    hh

--- script.sh
+++ script.sh
@@ -10,10 +10,14 @@ python -V
 
 pwd
 
-#wget -c https://download-installer.cdn.mozilla.net/pub/firefox/releases/67.0.4/linux-x86_64/zh-CN/firefox-67.0.4.tar.bz2
-#tar -xvf firefox-67.0.4.tar.bz2
-apt-get update
-apt-get install firefox
+wget -c https://download-installer.cdn.mozilla.net/pub/firefox/releases/67.0.4/linux-x86_64/zh-CN/firefox-67.0.4.tar.bz2
+tar -xvf firefox-67.0.4.tar.bz2
+
+rm -rf /usr/lib64/firefox
+mv firefox /usr/lib64
+rm /usr/bin/firefox
+ln -s /usr/lib64/firefox/firefox /usr/bin/firefox
+
 pip3 install selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
 
