babd75d2dfae0061d4925d06c2681038fd839c3a
王昆朋 <kunpeng.wang@luckincoffee.com>
2019-07-04 17:39:47 +0800

    hh

--- script.sh
+++ script.sh
@@ -9,8 +9,8 @@ python -V
 
 pwd
 
-wget -c https://download-installer.cdn.mozilla.net/pub/firefox/releases/67.0.4/linux-x86_64/zh-CN/firefox-67.0.4.tar.bz2
-tar -xvf firefox-67.0.4.tar.bz2
+wget -c wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/59.0.2/linux-x86_64/en-US/firefox-59.0.2.tar.bz2
+tar -xvf firefox-59.0.2.tar.bz2
 
 rm -rf /usr/lib64/firefox
 mv firefox /usr/lib64
