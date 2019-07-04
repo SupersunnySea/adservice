e4892ca387214da716ee55c3b26fdbb9d5f906c0
王昆朋 <kunpeng.wang@luckincoffee.com>
2019-07-04 15:43:14 +0800

    hh

--- script.sh
+++ script.sh
@@ -11,8 +11,8 @@ python -V
 pwd
 
 pip3 install selenium
-wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz
 
-tar -zxvf geckodriver-v0.24.0-linux64.tar.gz
+tar -zxvf geckodriver-v0.15.0-linux64.tar.gz
 
 ./microservices1.py
